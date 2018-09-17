c7727003de0adfb16d2216dc86e9d199df83699f
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2018-09-17 11:15:10 -0400

    Added SDK integration

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+echo "STARTED PRE BUILD"
+
 if [ "$APPCENTER_BRANCH" != "master" ];
 then
     plutil -replace CFBundleName -string "\$(PRODUCT_NAME) Beta" $APPCENTER_SOURCE_DIRECTORY/ios/ReactNativeBuildPipeline/Info.plist
@@ -7,4 +9,6 @@ fi
 
 echo "Changing ios bundle identifier"
 plutil -replace CFBundleIdentifier -string "io.rangle.reactnativepipeline" $APPCENTER_SOURCE_DIRECTORY/ios/ReactNativeBuildPipeline/Info.plist
+
+echo "FINISHED PRE BUILD"
 
---
+++ ios/Pods/Target Support Files/Pods-ReactNativeBuildPipeline/Pods-ReactNativeBuildPipeline-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ReactNativeBuildPipeline : NSObject
+@end
+@implementation PodsDummy_Pods_ReactNativeBuildPipeline
+@end
